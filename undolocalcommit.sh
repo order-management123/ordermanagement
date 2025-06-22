2ca499466d29e1596b6a89fddfff1bb83a3f352e
Mamatha1718 <mamathabandi1718@gmail.com>
2025-06-22 12:33:12 +0530

    this file is from --soft HEAD~1 this command move file from local repo to staging area

---
+++ undolocalcommit.sh
@@ -0,0 +1,9 @@
+this file i change for checking undo local commit but keep changes
+using git reset --soft HEAD~1
+
+if you use revert it delete all changes 
+so use git reset --soft HEAD~1
+it keeps changes not delete or remove changes
+
+yeah this is working i enter command it move from local to staging area commit back
+i check befor doing this git log and after git log it reset all
